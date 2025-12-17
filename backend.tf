42961a1bc8c540cbf3f82e1629e2ee308c952dab
Lorry Lachenal <lorry.lachenal@std.heh.be>
2025-12-17 16:17:44 +0100

    fix

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-lorrylach-2025"  
+    bucket         = "terraform-state-lorrylach-2025v2"  
     key            = "project/terraform.tfstate"
     region         = "us-east-1" 
     encrypt        = true
