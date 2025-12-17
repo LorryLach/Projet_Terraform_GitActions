e8d17e3735aeb157593e86bf86baac83823df509
Lorry Lachenal <lorry.lachenal@std.heh.be>
2025-12-17 10:35:18 +0100

    fix

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "terraform-state-lorrylach-2025"  
     key            = "project/terraform.tfstate"
     region         = "us-east-1" 
-    dynamodb_table = "terraform-state-locks"
     encrypt        = true
   }
 }
